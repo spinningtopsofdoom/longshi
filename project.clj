062fdf1e3a5f50885f8196badd001415ff1901af
Devin Walters <devinw@gmail.com>
2014-01-21 15:23:33 -0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject longshi "0.1.0-SNAPSHOT"
+  :description "Fressian for ClojureScript"
+  :url "http://github.com/devn/longshi"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2138"]]
+  :plugins [[lein-cljsbuild "1.0.1"]]
+  :source-paths ["src"]
+  :cljsbuild { 
+    :builds [{:id "longshi"
+              :source-paths ["src"]
+              :compiler {
+                :output-to "longshi.js"
+                :output-dir "out"
+                :optimizations :none
+                :source-map true}}]})
---
+++ src/longshi/core.cljs
@@ -0,0 +1,5 @@
+(ns longshi.core)
+
+(enable-console-print!)
+
+(println "Hello world!")

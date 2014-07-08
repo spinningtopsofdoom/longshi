a0cdbea4882c9727240485ed1babc5cf735d05ef
spinningtopsofdoom <dev+spinningtopsofdoom@bendyworks.com>
2014-07-08 13:18:30 -0500

    Dependpency bump for clojurescript
    Signed-off-by: Peter Schuck <peter@bendyworks.com>

--- project.clj
+++ project.clj
@@ -2,14 +2,15 @@
   :description "Fressian for ClojureScript"
   :url "http://github.com/devn/longshi"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]]
-  :plugins [[lein-cljsbuild "1.0.1"]]
+                 [org.clojure/clojurescript "0.0-2202" :scope "provided"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
+  :jvm-opts ["-Xmx1g" "-XX:+UseConcMarkSweepGC"]
   :source-paths ["src"]
-  :cljsbuild { 
-    :builds [{:id "longshi"
-              :source-paths ["src"]
-              :compiler {
-                :output-to "longshi.js"
-                :output-dir "out"
-                :optimizations :none
-                :source-map true}}]})
+  :cljsbuild {
+              :builds [{:id "longshi"
+                        :source-paths ["src"]
+                        :compiler {
+                                   :output-to "longshi.js"
+                                   :output-dir "out"
+                                   :optimizations :none
+                                   :source-map true}}]})

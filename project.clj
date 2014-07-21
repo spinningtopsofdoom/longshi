be3c1731488f2f24dbc2cf2d128fbf0bb8ca285f
spinningtopsofdoom <dev+spinningtopsofdoom@bendyworks.com>
2014-07-21 11:06:57 -0500

    Dependency bump for ClojureScript (to 2234)

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Fressian for ClojureScript"
   :url "http://github.com/spinningtopsofdoom/longshi"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2202" :scope "provided"]]
+                 [org.clojure/clojurescript "0.0-2234" :scope "provided"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :jvm-opts ["-Xmx1g" "-XX:+UseConcMarkSweepGC"]
   :source-paths ["src"]

abe5734e5e9bd71390bfad01960c2d9832ce3c58
spinningtopsofdoom <dev+spinningtopsofdoom@bendyworks.com>
2014-07-21 15:24:57 -0500

    Version bump to 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject longshi "0.1.1"
+(defproject longshi "0.1.2"
   :description "Fressian for ClojureScript"
   :url "http://github.com/spinningtopsofdoom/longshi"
   :dependencies [[org.clojure/clojure "1.5.1"]

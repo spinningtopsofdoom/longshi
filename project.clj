dd4114d1b923366953c169b34834a72df2ccbe4f
spinningtopsofdoom <dev+spinningtopsofdoom@bendyworks.com>
2014-10-24 13:58:56 -0500

    Bumped dependency versions
    Bumped versions for clojure, clojurescript, and transit-cljs

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject longshi "0.1.4"
   :description "Fressian for ClojureScript"
   :url "http://github.com/spinningtopsofdoom/longshi"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2234" :scope "provided"]]
-  :profiles {:bench {:dependencies [[com.cognitect/transit-cljs "0.8.161"]]}}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2371" :scope "provided"]]
+  :profiles {:bench {:dependencies [[com.cognitect/transit-cljs "0.8.188"]]}}
   :plugins [[lein-cljsbuild "1.0.3"]]
   :jvm-opts ["-Xmx1g" "-XX:+UseConcMarkSweepGC"]
   :source-paths ["src"]

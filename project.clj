aae9c2a678062117df04aff0510bd55383e10141
Peter Schuck <peter@bendyworks.com>
2015-08-09 13:02:49 -0500

    CLJS version bump and opts update

--- project.clj
+++ project.clj
@@ -1,21 +1,21 @@
 (defproject longshi "0.1.4"
   :description "Fressian for ClojureScript"
   :url "http://github.com/spinningtopsofdoom/longshi"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2371" :scope "provided"]]
-  :profiles {:bench {:dependencies [[com.cognitect/transit-cljs "0.8.188"]]}}
-  :plugins [[lein-cljsbuild "1.0.3"]]
-  :jvm-opts ["-Xmx1g" "-XX:+UseConcMarkSweepGC"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.48" :scope "provided"]]
+  :profiles {:bench {:dependencies [[com.cognitect/transit-cljs "0.8.220"]]}}
+  :plugins [[lein-cljsbuild "1.0.6"]]
+  :jvm-opts ^:replace  ["-Xms512m" "-Xmx512m" "-server"]
   :source-paths ["src"]
   :cljsbuild {
               :builds [{:id "benchmark"
                         :source-paths ["src" "sample" "benchmark"]
                         :compiler {:output-to "target/bench/longshi_bench.js"
                                    :optimizations :advanced}}
-                       {:id "perfomance"
+                       {:id "performance"
                         :source-paths ["src" "sample" "perf"]
                         :compiler {:output-to "target/perf/longshi_perf.js"
-                                   :optimizations :whitespace
+                                   :optimizations :simple
                                    :source-map "target/perf/longshi_perf.js.map"
                                    :output-dir "target/perf"
                                    :static-fns true
